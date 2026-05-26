525244a4d5243204c7507861372255677c34a4a4
WiniciusSantosLoebens <wsloebens@minha.fag.edu.br>
2026-05-25 21:45:06 -0300

    Delete demo directory

--- demo/src/main/java/com.exemplo.auditor/adapters/out/database/SpringDataAuditRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.auditor.adapters.out.database;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface SpringDataAuditRepository extends JpaRepository<AuditLogEntity, UUID> {
-}
--- demo/src/main/java/com.exemplo.auditor/ports/out/AuditRepositoryPort.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.auditor.ports.out;
-
-import com.exemplo.auditor.domain.model.AuditLog;
-
-public interface AuditRepositoryPort {
-    void save(AuditLog auditLog);
-}
